83db2237a99fd6e1af13cb5cfa53e7a0526da19e
Anna Sakivska <annasakivska@gmail.com>
2020-09-16 06:07:42 +0300

    validation form attempt

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
+import "./Registration.css";
+import eye from "./img/icons8-closed-eye-40.png";
+
+const validEmailRegex = RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+);
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach((val) => val.length > 0 && (valid = false));
+  return valid;
+};
+
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: null,
+      password: null,
+      errors: {
+        email: "",
+        password: "",
+      },
+    };
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;
+
+    switch (name) {
+      // case 'fullName':
+      //   errors.fullName =
+      //     value.length < 5
+      //       ? 'Full Name must be at least 5 characters long!'
+      //       : '';
+      //   break;
+      case "email":
+        errors.email = validEmailRegex.test(value) ? "" : "Email is not valid!";
+        break;
+      case "password":
+        errors.password =
+          value.length < 8
+            ? "Password must be at least 8 characters long!"
+            : "";
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ errors, [name]: value });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (validateForm(this.state.errors)) {
+      console.info("Valid Form");
+    } else {
+      console.error("Invalid Form");
+    }
+  };
+  render() {
+    const { errors } = this.state;
+    return (
+      <form
+        action="#"
+        method="post"
+        id="form"
+        class="seobase__registration"
+        class="form"
+        onSubmit={this.handleSubmit}
+        noValidate
+      >
+        <label for="email" class="form__email">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Enter your email"
+          onChange={this.handleChange}
+          noValidate
+        />
+        {errors.email.length > 0 && (
+          <span className="error">{errors.email}</span>
+        )}
+        <label for="password" class="form__password">
+          Password
+        </label>
+        <h3 class="form__forgot-password">Forgot Password?</h3>
+        <img class="form__password-icon" src={eye}></img>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter password"
+          onChange={this.handleChange}
+          noValidate
+        />
+        {errors.password.length > 0 && (
+          <span className="error">{errors.password}</span>
+        )}
+      </form>
+    );
+  }
+}
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Registration.css";
+import Form from "../Form/Form.jsx";
 import logo from "./img/logo_white.svg";
-import eye from "./img/icons8-closed-eye-40.png";
 import { Link } from "react-router-dom";
 
 class Registration extends React.Component {
@@ -13,28 +13,7 @@ class Registration extends React.Component {
           <h1 class="seobase__title">Log in</h1>
           <div class="seobase__registration-container">
             <div class="seobase__form-wrapper">
-              <form
-                action="#"
-                method="post"
-                id="form"
-                class="seobase__registration"
-                class="form"
-              >
-                <label for="email" class="form__email">
-                  Email
-                </label>
-                <input type="text" id="email" placeholder="Enter your email" />
-                <label for="password" class="form__password">
-                  Password
-                </label>
-                <h3 class="form__forgot-password">Forgot Password?</h3>
-                <img class="form__password-icon" src={eye}></img>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Enter password"
-                />
-              </form>
+              <Form />
             </div>
             <Link to="/home">
               <button type="submit" value="Submit" class="form__submit">
